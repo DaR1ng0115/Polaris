8efaa80dbc24928110bcbc17b6b10647a7de1b1e
dar1ng <2817619539@qq.com>
2026-07-11 03:07:17 +0000

    为matrix.cpp添加了部分注释

--- include/matrix.h
+++ include/matrix.h
@@ -28,28 +28,21 @@ private:
 // 二维矩阵是简单且易懂的一种matrix实现方式，但对于大于二维的张量，表示会略显复杂，比如在卷积神经网络中，彩色图片有这些参数：数量Batch，
 // 通道channels，图片尺寸height和width。因此对于彩色图片，二维矩阵的表达形式为(Batch*hight*width, channels)(通道优先)
 // 但无论实现方式如何，本质是相同，均为内存上一段连续的一维数组。
+// 此处先实现二维矩阵，以便读者能更好的理解和学习
 
-// A two-dimensional matrix is a simple and easy-to-understand implementation of a tensor, but for tensors greater than two dimensions, 
-// the representation can be somewhat complex. For example, in convolutional neural networks, color images have parameters such as Batch size, 
-// channels, height, and width. Therefore, for color images, the two-dimensional matrix representation is (Batch*height*width, channels) (channel-first).
     int rows_;
     int cols_;
     std::vector<float> data_;
 public:
 // 思考：在构造matrix的时候，你可能需要哪些构造方式？这决定了你如何定义构造函数。
 
-// Consider: When constructing a matrix, what construction methods might you need? This determines how you define the constructor.
     Matrix();
     Matrix(int rows, int cols);
     Matrix(int rows, int cols, float fill_data);
 
 // 此处是读取/修改matrix的方法，同样思考，你可能会如何使用一个matrix，你希望它具备什么功能？
-// 如果没有头绪，可以参照此处的函数，但强烈建议你尽可能自己思考：不是方法催生功能，而是功能催生方法
+// 如果没有头绪，可以参照此处的函数，但强烈建议你尽可能自己思考：不是函数催生功能，而是功能催生函数
 
-// Here are the methods for reading/modifying the matrix. Similarly, 
-// think about how you might use a matrix and what functions you would like it to have.
-// If you have no idea, you can refer to the functions here, but it is strongly recommended that you think for yourself as much as possible:
-// it is not the methods that give rise to the functions, but the functions that give rise to the methods.
     int rows() const;
     int cols() const;
     int length() const;
@@ -58,7 +51,6 @@ public:
 
 // 此处是重载运算符，是支撑matrix逻辑运算的重要组成部分
 
-// Here are the overloaded operators, which are an important part of supporting matrix logical operations.
     Matrix operator+(const Matrix &other) const;
     Matrix& operator=(const Matrix &other);
     float& operator()(int rows_idx, int cols_idx);
